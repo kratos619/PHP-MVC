634d0e89bab554a1117ae558acfe002473450599
kratos619 <gaurav.palaspagar@hotmail.com>
2018-08-06 23:32:16 +0530

    Add PDO refrence file

---
+++ dop-refrence.php
@@ -0,0 +1,36 @@
+<?php 
+
+$host = 'localhost';
+$user = 'root';
+$password = null;
+$dbname = 'pdo_test';
+
+//set dns
+
+$dns = 'mysql:host='.$host . ';dbname=' .$dbname;
+
+// create PDO instance
+$pdo = new PDO($dns,$user,$password);
+$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,PDO::FETCH_OBJ);
+
+/* squery selrch from id
+$id = 1 ;
+$sql = 'select * from user where id = :id';
+$stmt = $pdo->prepare($sql);
+$stmt->execute(['id' => $id]);
+$users = $stmt->fetchAll();
+
+foreach($users as $user){
+    echo $user->name;
+}
+*/
+
+// entering data 
+
+$name = "rahul";
+$email = 'rgamil.com';
+$role = "Admin";
+$sql = 'INSERT INTO USER(name,email,role) values(:name,:email,:role)';
+$stmt = $pdo->prepare($sql);
+$stmt->execute(['name'=>$name,'email'=>$email,'role'=>$role]);
+echo 'added Users';
