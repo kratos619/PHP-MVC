547327c39439f937c064f10dd2beeb86f7a7f50d
kratos619 <gaurav.palaspagar@hotmail.com>
2018-08-11 14:00:16 +0530

    Add Registration and Login markup and validation

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -3,10 +3,64 @@ class Users extends Controller {
     public function __construct(){
 
     }
+
+
     public function register(){
         //check for Post
-        if($_SERVER['REQUEST_METHOD'] == 'post'){
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
             //process form
+            //sanitize POST data
+            
+            $_POST = filter_input_array(INPUT_POST,FILTER_SANITIZE_STRING);
+            $data = [
+                'name'=> trim($_POST['name']),
+                'email'=> trim($_POST['email']),
+                'password'=> trim($_POST['password']),
+                'confirm_password'=> trim($_POST['confirm_password']),
+                'name_error'=> '',
+                'email_error'=> '',
+                'password_error'=> '',
+                'confirm_password_error'=> ""
+            ];
+
+            // validate email
+            if(empty($data['email'])){
+                $data['email_error'] = "Please Enter Email";
+            }
+            
+            // validate name
+            if(empty($data['name'])){
+                $data['name_error'] = "Please Enter Name";
+            }
+            // validate password
+            if(empty($data['password'])){
+                $data['password_error'] = "Please Enter Password";
+            }
+            
+            // validate password
+            if(empty($data['password'])){
+                $data['password_error'] = "Please Enter Password";
+            }elseif(strlen($data['password']) < 6 ){
+                $data['password_error'] = "Password At List 6 Character";
+            }
+
+            
+            // validateconfirl
+            if(empty($data['confirm_password'])){
+                $data['confirm_password_error'] = "Please Enter Confirm Password";
+            }elseif($data['confirm_password'] != $data['password']){
+                $data['confirm_password_error'] = "Password  not match";
+            }
+
+            // make sure errors are empty
+            if(empty($data['email_error']) && empty($data['confirm_password_error']) && empty($data['name_error']) && empty($data['password_error'])){
+                //validate()
+                die('SUCCESS');
+            }else{
+                //load view with error
+                $this->view('users/register',$data);
+            }
+
         }else{
             //init data
             $data = [
@@ -16,12 +70,56 @@ class Users extends Controller {
                 'confirm_password'=> '',
                 'name_error'=> '',
                 'email_error'=> '',
-                'password_error'=> ' ',
-                'confirm_password'=> ""
+                'password_error'=> '',
+                'confirm_password_error'=> ""
             ];
         }
 
         $this->view('users/register',$data);
+    }
+
+    
+    public function login(){
+        //check for Post
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            //process form
+            $_POST = filter_input_array(INPUT_POST,FILTER_SANITIZE_STRING);
+              $data = [
+                
+                'email'=> trim($_POST['email']),
+                'password'=> trim($_POST['password']),
+                'email_error'=> '',
+                'password_error'=> ''
+            ];
+
+                // validate email
+            if(empty($data['email'])){
+                $data['email_error'] = "Please Enter Email";
+            }
+                // validate email
+            if(empty($data['password'])){
+                $data['password_error'] = "Please Enter pssword";
+            }
+            //make sure error are empty
+            if(empty($data['email_error']) && empty($data['email_error'])){
+              die('SUCCESS');
+                
+            }else{
+                $this->view('users/login',$data);
+            }
+
+        }else{
+            //init data
+            $data = [
+                
+                'email'=> '',
+                'password'=> '',
+                'email_error'=> '',
+                'password_error'=> ''
+            ];
+        }
+
+        $this->view('users/login',$data);
     }
 }
 
--- app/views/users/login.php
+++ app/views/users/login.php
@@ -2,14 +2,9 @@
  <div class="row">
     <div class="col-md-6 mx-auto">
         <div class="card card-body bg-light mt-5">
-             <h2>create an Account</h2>
-             <p>Please Fill Out this Form To Register With Us</p>
-                    <form method="POST" action="<?php echo URLROOT; ?>/users/register">
-                        <div class="form-group">
-                            <label for="name">Name: <sup>*</sup></label>
-                            <input type="text" name="name" class="form-control form-control-lg <?php echo (!empty($data['name_error'])) ? 'is-invalid' : '';?>" value="<?php $data['name']; ?>" />
-                            <span><?php echo $data['name_error']; ?></span>
-                        </div>
+             <h2>LogIn</h2>
+             <p>Please Fill Out this Form To Login  With Us</p>
+                    <form method="POST" action="<?php echo URLROOT; ?>/users/login">
                         <div class="form-group">
                             <label for="email">Email: <sup>*</sup></label>
                             <input type="email" name="email" class="form-control form-control-lg <?php echo (!empty($data['email_error'])) ? 'is-invalid' : '';?>" value="<?php $data['email']; ?>" />
@@ -20,17 +15,13 @@
                             <input type="password" name="password" class="form-control form-control-lg <?php echo (!empty($data['password_error'])) ? 'is-invalid' : '';?>" value="<?php $data['password']; ?>" />
                             <span><?php echo $data['password_error']; ?></span>
                         </div>
-                        <div class="form-group">
-                            <label for="confirm_password">Confirm Password: <sup>*</sup></label>
-                            <input type="text" name="confirm_password" class="form-control form-control-lg <?php echo (!empty($data['confirm_password_error'])) ? 'is-invalid' : '';?>" value="<?php $data['confirm_password']; ?>" />
-                            <span><?php echo $data['confirm_password_error']; ?></span>
-                        </div>
+
                         <div class="row">
                             <div class="col">
-                                <input type="submit" value="Register" class="btn btn-success btn-block">
+                                <input type="submit" value="Login" class="btn btn-success btn-block">
                             </div>
                             <div class="col">
-                                <a href="<?php echo URLROOT; ?>/users/login" class="btn btn-success btn-block">Have an account? login </a>
+                                <a href="<?php echo URLROOT; ?>/users/register" class="btn btn-success btn-block">Register With Us </a>
                             </div>
                         </div>
                     </form>
